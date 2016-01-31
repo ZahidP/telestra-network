eb543e6a2c2528009b7d1ab93060575fbff8b706
Zahid Panjwani <zahidpanjwani@Zahids-MacBook-Pro.local>
2016-01-30 23:24:50 -0600

    so many histograms

--- py/main.py
+++ py/main.py
@@ -14,23 +14,28 @@ def extract():
     train = pd.read_csv('../data/train.csv', index_col='id')
     resource = pd.read_csv('../data/resource_type.csv')
     log_feature = pd.read_csv('../data/log_feature.csv')
+    severity_type = pd.read_csv('../data/severity_type.csv')
     event = pd.read_csv('../data/event_type.csv')
     result = pd.merge(resource, event, how="left",on="id")
     results = pd.merge(result,log_feature, how="left")
     #results = results.set_index(['id'])
     results = pd.merge(train,results,left_index="true", how="left",right_on="id")
     # results = pd.merge(train,results,left_index="true", right_index="true", how="left")
-
-
     # results_2 = pd.merge(train,results,left_index="true", right_index="right" how="left", on="id")
     return results
 
-def summarize():
+def slice_n_dice(train):
+    # variable creation
+
+
+
+def summarize(results):
     # get number of unique locations
     # number of unique features
     # histogram of # of events by location
     locations, event_types = results.location.ravel(), results.event_type.ravel()
     ids, resources = results.id.ravel(), results.resource_type.ravel()
+    fault_severity = results.fault_severity.ravel()
     unique_loc = pd.Series(locations).unique()
     unique_ev_type = pd.Series(event_types).unique()
     unique_id = pd.Series(ids).unique()
@@ -46,6 +51,8 @@ def summarize():
         unique_locs = pd.Series(ids).unique()
         uniques_loc_id.append(len(unique_locs))
         # unique_counts = [ (i,unique_id.count(i)) for i in set(unique_id) ]
+
+    # for each unique id
     for jj in range(0,len(unique_id)):
         # subset based on id
         subset2 = results.loc[results.id==unique_id[jj]]
@@ -64,16 +71,21 @@ def everything():
     # test set
     # validation set
 
-def histogram(x,title):
+# pass in array of x per y, where x is the count per bucket
+def histogram(x,title,use_np,norm):
     # mu, sigma = 100, 15
     # x = mu + sigma*np.random.randn(10000)
     # the histogram of the data
+    title = str(title)
+    title = "More Histograms"
+    if use_np:
+        x = np.bincount(x)
     mu = np.average(x)
-    sigma = np.std(uniques)
+    #sigma = np.std(uniques)
     td = dtime.date.today()
     dnow = dtime.date.isoformat(td)
     bin_count = int(np.max(x)/2.25)
-    n, bins, patches = plt.hist(x, 8, normed=1, facecolor='green', alpha=0.75)
+    n, bins, patches = plt.hist(x, 8, normed=norm, facecolor='green', alpha=0.75)
     # add a 'best fit' line
     # y = mlab.normpdf(bins, mu, sigma)
     # l = plt.plot(bins, y, 'r--', linewidth=1)
@@ -91,9 +103,23 @@ def histogram(x,title):
     title2 = map(shorten, title)
     title_file = '-'.join(list(title2))
     plt.title(r'$\mathrm{Histogram\ of:\ ' + title_hist + '}\ $')
-    plt.axis([0, np.max(x), 0, 0.70])
+    if norm:
+        max_y = 1
+    else:
+        max_y = len(x) *.8
+    plt.axis([0, np.max(x), 0, max_y])
     plt.grid(True)
     #limit file name length
     title_file = title_file[0:14]
     fname = '../visualizations/' + str(title_file) + '-' + str(dnow) + '.png'
     plt.savefig(fname)
+    plt.show()
+
+
+def counts(unique,bin,items,df,counts):
+  for ii in range(0,len(unique)):
+    subset = df.loc[df.col == unique[ii]]
+    item_list = subset.items.ravel().tolist()
+    u_item_list = pd.Series(item_list).unique()
+    counts.append(len(u_item_list))
+    return counts
